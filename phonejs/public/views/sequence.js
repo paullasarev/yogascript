e15b3fd7a4d484758f3039a9caf1813f593c8f8d
paullasarev@gmail.com <paullasarev@gmail.com@f2317d72-843d-f979-c47e-eb441419b656>
2013-11-10 12:40:45 +0000

    switch to DataSource binding

--- phonejs/public/views/sequence.js
+++ phonejs/public/views/sequence.js
@@ -1,4 +1,10 @@
 ﻿YogaScript.sequence = function (params) {
   return {
+    dataSource: [
+      { id: 1, name: 'Pranamasana' },
+      { id: 2, name: 'Hasta Uttanasana' },
+      { id: 3, name: 'Hastapaadasana' },
+      { id: 4, name: 'Aekpaadprasarnaasana ' },
+    ],
   };
 };
--- phonejs/public/views/sequence_list.js
+++ phonejs/public/views/sequence_list.js
@@ -1,4 +1,9 @@
 ﻿YogaScript.sequence_list = function (params) {
   return {
-  };
+    dataSource: [
+      { id: 1, name: 'Surya Namascar 1' },
+      { id: 2, name: 'Surya Namascar 2' },
+      { id: 3, name: 'Chandra Namascar' },
+    ],
+  };  
 };
