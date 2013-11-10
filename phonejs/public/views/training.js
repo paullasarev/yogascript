d242bd3acdefa6b91b0d0555586eb079bc1a1966
paullasarev@gmail.com <paullasarev@gmail.com@f2317d72-843d-f979-c47e-eb441419b656>
2013-11-10 13:05:55 +0000

    add moment.js

--- phonejs/public/views/training.js
+++ phonejs/public/views/training.js
@@ -5,11 +5,14 @@
         return this.isStarted ? "START" : "STOP";
       },
       modeMessage: ko.observable("START"),
+      
       startStop: function() {
         var msg = this.getMessage();
         this.isStarted = !this.isStarted;
         this.modeMessage(msg);
         YogaScript.notify(this.getMessage());
       },
+      
+      trainingDate: moment().format('ddd, DD MMM, hA'),
     };
 };
