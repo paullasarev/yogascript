b2d0dda4110cad48514438c5da339277c3b8927b
trefilova.yulya@gmail.com <trefilova.yulya@gmail.com@f2317d72-843d-f979-c47e-eb441419b656>
2013-11-14 13:55:57 +0000

    Add description & duration to poses

--- trunk/phonejs/public/index.js
+++ trunk/phonejs/public/index.js
@@ -90,13 +90,13 @@ YogaScript.home = function () {
 		{id:7, name:'P17', level:3, durMin:'2m', durMax:'5m', url:'images/download.png'}
 	];
   var myposes =  [
-    {id:0, name:'Sun salutation', level:2, durMin:'2m', durMax:'5m', url:'images/SN1_1.png'},
-    {id:1, name:'Janubhalasana', level:2, durMin:'2m', durMax:'5m', url:'images/SN1_2.png'},
-    {id:2, name:'Ardhabhujangasana', level:2, durMin:'2m', durMax:'5m', url:'images/SN1_3.png'},
-    {id:3, name:'Hastapadasana', level:2, durMin:'2m', durMax:'5m', url:'images/SN1_4.png'},
-    {id:4, name:'Ashtangasana', level:2, durMin:'2m', durMax:'5m', url:'images/SN1_5.png'},
-    {id:5, name:'Bhujangasana', level:2, durMin:'2m', durMax:'5m', url:'images/SN1_6.png'},
-    {id:6, name:'Adho Mukha Svanasana', level:2, durMin:'2m', durMax:'5m', url:'images/SN1_7.png'},
+    {id:0, name:'Extended Mountain pose', level:2, durMin:'2m', durMax:'5m', url:'images/SN1_1.png', desc: "Inhale through your nose and raise your arms up to the side, palms facing upwards. Extend your arms above your head with your hands in prayer position."},
+    {id:1, name:'Janubhalasana', level:2, durMin:'2m', durMax:'5m', url:'images/SN1_2.png', desc: "Swan dive to forward bend, be sure to bend your knees especially if you have tight hamstrings. This will protect the back: Exhale through your nose. Open your arms wide and bend at your waist to a standing forward bend. Your hands should touch the floor if you can or if not, the front of the ankles."},
+    {id:2, name:'Ardhabhujangasana', level:2, durMin:'2m', durMax:'5m', url:'images/SN1_3.png', desc: "Step with your left foot in front of your right foot and lean slightly on it and bend your knees forwards, without lifting your right foot. Your right foot should be almost off the ground, with just the toes touching."},
+    {id:3, name:'Hastapadasana', level:2, durMin:'2m', durMax:'5m', url:'images/SN1_4.png', desc: "Plank pose: Inhale and take your shoulders forward directly over your wrists, extending well with your arms to form plank position. Keep your thighs strong and firm, your feet flexed and your belly drawn in."},
+    {id:4, name:'Ashtangasana', level:2, durMin:'2m', durMax:'5m', url:'images/SN1_5.png', desc: "Knees-chest-chin pose: Exhale and bend your knees to the floor and then lower your chest and chin to the floor. Keep your chest open and your elbows close to the side of your ribcage."},
+    {id:5, name:'Bhujangasana', level:2, durMin:'2m', durMax:'5m', url:'images/SN1_6.png', desc: "Cobra: Inhale and raise your upper body to the cobra pose. Roll your shoulders back and extend the shoulder blades down and press them in towards the chest. Your chest should be lifted and open and elbows should stay close to the body. Make sure to lift up your knee caps and firm your thighs. Your legs and feet should be well extended. As a variation, you can also do an upward dog."},
+    {id:6, name:'Adho Mukha Svanasana', level:2, durMin:'2m', durMax:'5m', url:'images/SN1_7.png', desc: "Downward-facing dog: Spread your fingers and press your palms into the mat, they should be shoulder width apart. Now, lift your hips up towards the sky, lengthening your spine. Gently straighten your legs, pressing your heels down into your mat as far as you can go."},
   ];
 	var secus =  [
 		{id:0, name:'Surya 0', poses:[0,1]},
