92066b660c543c67d669760cf8b4f35f90165731
paullasarev@gmail.com <paullasarev@gmail.com@f2317d72-843d-f979-c47e-eb441419b656>
2013-11-16 09:37:40 +0000

    move common routing code to utils.js

--- trunk/phonejs/routes/sequences.js
+++ trunk/phonejs/routes/sequences.js
@@ -1,23 +1,11 @@
 var async = require('async');
 var fs = require('fs');
 var path = __dirname + '/../data/sequences';
+var utils = require("./utils");
+
 var CachedResults = null;
 
-function readJsonAsync(file, callback) {
-  console.log('readJsonAsync file: ' + file);
-  fs.readFile(path + '/' + file, 'utf8', function(err, data){
-    if (err){
-      console.log('readFile  error: ' + err + ' path: ' + path);
-      return;
-    }
-    
-    //console.log('data: ' + data);
-    var json = JSON.parse(data);
-    callback(err, json);
-  });
-};
-
-var sendResults = function(res, theResults, searchString, skip, take) {
+function sendResults(res, theResults, searchString, skip, take) {
   var results = theResults.slice(0);
   if (searchString)
   {
@@ -37,24 +25,6 @@ exports.clearCache = function(){
   CachedResults = null;
 }
 
-load = function(callback) {
-  fs.readdir(path, function(err, files) {
-    if (err){
-      console.log('readdir error: ' + err + ' path: ' + path);
-      return;
-    }
-
-    var jsonFiles = files.filter(function(v){ return /\.json/.test(v); });
-    jsonFiles.sort();
-    async.map(jsonFiles, readJsonAsync, function(err, results) {
-      // results = ['file 1 content', 'file 2 content', ...]
-      CachedResults = results;
-      console.log("load: " + CachedResults.length);
-      callback();
-    });
-  });
-};
-
 exports.sequences = function(req, res){
   var skip = req.query.skip;
   var take = req.query.take;
@@ -66,25 +36,15 @@ exports.sequences = function(req, res){
   }
   else
   {
-    load(function(){
-      sendResults(res, CachedResults, searchString, skip, take);
+    utils.load(CachedResults, function(results){
+      sendResults(res, results, searchString, skip, take);
     });
   }
 };
 
-function findById(source, id) {
-  for (var i = 0; i < source.length; i++) {
-    if (source[i].id == id) {
-      return source[i];
-    }
-  }
-  
-  return null;
-}
-
 sendOne = function(res, results, id) {
   console.log("sendOne: id=" + id);
-  var obj = findById(results, id);
+  var obj = utils.findById(results, id);
   if (obj)
   {
     console.log("sendOne: obj=" + obj);
@@ -104,8 +64,8 @@ exports.sequence_by_id = function(req, res){
   }
   else
   {
-    load(function(){
-      sendOne(res, CachedResults, id);
+    utils.load(CachedResults, function(results){
+      sendOne(res, results, id);
     });
   }
 };
