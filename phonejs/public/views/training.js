fec9a56a613533dda8b449207761bad02d341142
trefilova.yulya@gmail.com <trefilova.yulya@gmail.com@f2317d72-843d-f979-c47e-eb441419b656>
2013-11-16 13:16:57 +0000

    lookup

--- phonejs/public/views/training.js
+++ phonejs/public/views/training.js
@@ -1,9 +1,24 @@
 ﻿YogaScript.training = function (params) {
   var id = 1; //params.id
+  if (params.id) {
+    id = params.id;
+  }
+  var skip = 0;
+  var PAGE_SIZE = 10;
+  var elementsVisible = true;
+  var lookupVisible = false;
+
+  if (id < 0) {
+      elementsVisible = false; 
+      lookupVisible = true;
+  }
   
   var viewModel = {
       isStarted: false,
       id: id,
+      searchString: ko.observable(''),
+      elementsVisible: elementsVisible,
+      lookupVisible: lookupVisible,
       
       getMessage: function() {
         return this.isStarted ? "START" : "STOP";
@@ -22,6 +37,44 @@
       
       completionPercent: ko.observable(20),
       dataSource: ko.observable([]),
+      selectedId: ko.observable(),
+
+       processValueChange: function() {
+        this.id = this.selectedId;
+        if (this.id >= 0)
+        {
+          YogaScript.app.navigate("training/" + this.selectedId);
+        }
+      },
+
+      sequences: DevExpress.data.createDataSource({
+        load: function (loadOptions) {
+          if (loadOptions.refresh) {
+            skip = 0;
+          }
+          var deferred = new $.Deferred();
+          
+          $.get('/api/sequences',
+          {
+            skip: skip,
+            take: PAGE_SIZE,
+            searchString: viewModel.searchString()
+          })
+          .done(function (result) {
+            //Console.log(result);
+            skip += PAGE_SIZE;
+            var mapped = $.map(result, function (data) {
+              return {
+                name: data.name,
+                id: data.id
+              };
+            });
+            deferred.resolve(mapped);
+          });
+          return deferred;
+        }
+      })
+
   };
   
   $.get('/api/sequences/' + viewModel.id)
